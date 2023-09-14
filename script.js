a8f7977043e9ab72dea91c05201c86de6084a949
So Youn Choi <manghatuhyo@gmail.com>
2023-09-14 19:15:08 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Fetching weather data and converted to json.
+const getWeather = async () => {
+    try {
+        const response = await fetch("https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=966881349119fc14f6f3831c44ff9b53", { method: 'GET' });
+
+        const data = await response.json();
+
+        console.log(data)
+
+        const forecast = data.list[0];
+
+        document.getElementById("city-name").innerText = data.city.name;
+        document.getElementById("temperature").innerText = forecast.main.temp;
+        document.getElementById("weather").innerText = forecast.weather[0].main;
+    } catch {
+        console.error("Could not get weather data");
+    }
+}
+getWeather();
